7443531e1c6070d8fa60a99d1722a6452271bba7
Hugo Fernando <Huginho888@outlook.com>
2023-05-10 10:35:43 -0300

    Atualizando porta

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const port = process.env.PORT || 3333
+const port = process.env.PORT || process.env.port || 3333
 
 app.listen(port, (): void => {
   console.log('Server started in port ' + port)
